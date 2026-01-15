d0c1320a4fbeb52d9979c5cf61bde38d9e53dbd5
Pintu Sharma <pintu.sharma@empover.com>
2026-01-15 16:40:38 +0530

    latest code push

---
+++ python.py
@@ -0,0 +1,13 @@
+# first Program
+name = "Pintu Kumar Sharma"
+age = 26
+location = "Gopalganj Bihar"
+PhoneNumber = '+91:6207217249'
+
+print(name)
+print(age)
+print(location)
+print(PhoneNumber)
+
+print("My name is " + name)
+print("I am " + str(age) + " years old.")
---
+++ python.sh
@@ -0,0 +1,18 @@
+##!/bin/bash
+#
+#echo "Printing Python Version using shell script Commands"
+#python3 --version > testing.yml
+#
+#echo "Printing python file" >> testing.yml
+#python3 python.py >> testing.yml
+#
+##echo "learning.py file move python.py"
+##mv learning.py python.py
+#
+
+echo "Git Pushing Commands"
+git add .
+git commit -m "latest code push"
+git push origin main
+
+done
