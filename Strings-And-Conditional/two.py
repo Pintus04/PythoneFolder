8ecc01bf3fb9b3d224537fd7bb1d80d171bfc5bd
Pintu Sharma <pintu.sharma@empover.com>
2026-01-16 16:01:28 +0530

    Latest code push

---
+++ Strings-And-Conditional/two.py
@@ -0,0 +1,6 @@
+str = "I Reading Book for Clear the Exam."
+
+print(str.capitalize()) # Only Capital first letter in words 
+print(str.count("o")) #Count o in words
+print(str.replace("o","U"))  # Replace letter o with U
+print(str.replace("Book", "NoteBook")) # replaceing word
