3e8f7401bc82ae8eca787ed8b8dd29be1d300306
Pintu Sharma <pintu.sharma@empover.com>
2026-01-16 14:30:51 +0530

    Latest code push

---
+++ First_practice.py
@@ -0,0 +1,7 @@
+# Question: Write a Program to input 2 numbers & Print their sum
+
+x = int(input("Inter your first number: "))
+y = int(input("Inter your second Number: "))
+
+sum=x + y
+print(sum)
---
+++ Strings-And-Conditional/slicing.py
@@ -0,0 +1,22 @@
+# Accessing part of string
+
+str="Vishal is playing Cricket"
+#printin 0 to 5
+print(str[1:5])
+
+#printing 0 to 5 
+str1 = str[:5]
+print(str1)
+
+# printing after 4
+str2 = str[4:]
+print(str2)
+
+#printing length
+str3 = len(str)
+print(str3)
+
+#Only printing playing
+str4 = str[9:17]
+str5 = len(str4)
+print(str5)
