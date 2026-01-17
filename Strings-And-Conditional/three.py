ee07cac57eab97c6455566fef82ef0d7ba9d7bfd
Pintu Sharma <pintu.sharma@empover.com>
2026-01-17 10:51:51 +0530

    Latest code push

---
+++ Strings-And-Conditional/three.py
@@ -0,0 +1,20 @@
+# age = int(input("Enter Your age :"))
+
+# if age >= 18:
+#     print("You are able to vote.!")
+# else:
+#     print("You are under 18. You can't vote.!")
+
+# grade student based on marks
+
+marks = int(input("Inter Your marks : "))
+
+if marks >= 500:
+    print("Your grade is A")
+elif marks >= 400:
+    print("Your grade is B")
+elif marks >= 300:
+    print("Your grade is C")
+else:
+    print("You are failed... TRY NEXT TIME..")
+    
