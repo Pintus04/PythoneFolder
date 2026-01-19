9c73369439927727a0faf9e9e112be4b138ff506
Pintu Sharma <pintu.sharma@empover.com>
2026-01-19 10:16:44 +0530

    All latest code

---
+++ 2-Strings-And-Conditional-statements/three.py
@@ -0,0 +1,34 @@
+# age = int(input("Enter Your age :"))
+
+# if age >= 18:
+#     print("You are able to vote.!")
+# else:
+#     print("You are under 18. You can't vote.!")
+
+
+
+# # grade student based on marks
+# marks = int(input("Inter Your marks : "))
+# if marks >= 500:
+#     print("Your grade is A+")
+# elif marks >= 450:
+#     print("Your grade is A")
+# elif marks >= 400:
+#     print("Your grade is B+")
+# elif marks >= 350:
+#     print("Your grade is B")
+# elif marks >= 300:
+#     print("Your grade is C")
+# else:
+#     print("You are failed... TRY NEXT TIME..")
+
+
+Number = int(input(" Inter Your Number :"))
+
+rem = Number % 2 == 0
+
+
+if(rem == 0):
+    print("Even number")
+else:
+    print("ODD")
