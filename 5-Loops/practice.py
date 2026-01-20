98ec2716b5220cdde915b901342218f5d902f5d9
Pintu Sharma <pintu.sharma@empover.com>
2026-01-20 14:45:26 +0530

    latest cde

--- 5-Loops/practice.py
+++ 5-Loops/practice.py
@@ -2,12 +2,28 @@
 # for i in range(1, 101):
 #     print(i)
 
-i = 1
+# i = 1
 
-while i <= 100:
-    print(i)
-    i += 1
+# while i <= 100:
+#     print(i)
+#     i += 1
 
 # # Print even numbers from 100 to 1
 # for i in range(100, 0, -1):
 #         print(i)
+
+
+
+# write 3 table
+
+# i = 1
+# while i <= 10:
+#     print(99 * i)
+#     i += 1
+
+
+n = int(input("Enter a number to print its table: ")) 
+i = 1
+while i <= 10:
+    print(n * i)
+    i += 1
